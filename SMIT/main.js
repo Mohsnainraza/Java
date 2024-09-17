422364a7a3aa82bfd13a086bc9ec7f4592815c3d
Mohsnainraza <mohsnainraza210@gmail.com>
2024-09-17 19:15:59 +0500

    Add files via upload

---
+++ SMIT/main.js
@@ -0,0 +1,136 @@
+// Javascript
+// chapter 1 (Alert) 
+
+// alert("welcome")
+
+
+// chapter 2 (variable for String)
+
+//  a = 'Mohsnain';
+//  b = 'Raza';
+//  c = a + b; 
+//  console.log(c);
+
+
+// chapter 3 (variable for number)
+
+//  var a = 60;
+//  var b = 40;
+//  var c = a - b;
+//  console.log (c)
+
+//  var num = 100;
+//  number = num * 50; 
+//  console.log (number)
+
+
+ //chapter 4 (Variable Names Legal and Illegal)
+
+ //legal variable Names:
+
+//  let Name = 'Ali';
+//  let SurName = 'Hassan';
+//  let surname = Name + SurName;
+//  console.log(surname)
+
+//  let y_ear = 2024;
+//  var age = 20;
+//  let add2 = y_ear - age;
+//  console.log(add2)
+
+//  let amount$ = 2000;
+//  let Discount = amount$ - 300;
+//  console.log(Discount)  
+
+// Illegal Variable Names:
+
+// var my name = 'nazim';
+// var = 'Ahmed';
+// var else = my name + var;
+// console.log(var)
+
+// let 2number = 35;
+// let my-num = 20;
+// let my@variable = 2number 1st + my-num;
+// console.log(my@variable)
+
+
+// chapter 5 (Math expressions:Familiar operators)
+
+//  var a = 75 - 45
+//  console.log(a)
+
+//  var a = 65  
+//  var b = 25 
+//  console.log(a + b)
+
+//  var a = 17 
+//  var b = a * 7 
+//  console.log(b)
+
+//  var a = 14 
+//  var b = 112
+//  console.log(a/b)
+
+
+// chapter 6 (Math expressions:unfamiliar operators)
+
+//  var a = 95
+//  var b = a -- 35
+//  console.log(b)
+
+//  var a = 55 
+//  var b = 35
+//  console.log(a ++ b)
+
+ 
+//  var a = 44 
+//  var b = 25 
+//  console.log(a -+ b)
+
+//  var a = 85;
+//  var b = 77 +- a;
+//  console.log(b)
+
+
+// chapter 7 (Math expressions:eliminating ambiguity)
+
+//  var a = 2 
+//  var b = 2
+//  console.log(2 + a / b)
+//  var a = 66 
+//  var b = 22
+//  console.log(4 - a * b)
+
+//  var a = 66
+//  var b = 44
+//  console.log(6 * a + b)
+
+//  var a = 90
+//  var b = 80
+//  console.log(7 / a - b)
+
+
+// chapter 8 (Concatenating text strings)
+
+//  var name = "Mohsnain";
+//  alert("Thanks, " + name  + "!")
+
+
+//Promt (Chap 9)
+
+//  var question = prompt ("your nationality");
+//  var question2 = prompt ("your name");
+//  var spec = question + question2;
+
+
+// Simple Calculator Using Promt "
+
+//   var msg1 = prompt("Enter your 1st Number");
+//   var msg3 = Number(msg1);
+//   var msg2 = prompt("Enter your 2nd Number");
+//   var msg4 = Number(msg2);
+//   alert(msg3+msg4)
+//   alert('Hello');
+
+
